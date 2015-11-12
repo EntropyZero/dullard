37f8bc242458bdc4a96bdd50e552db66477eade9
Steven Buxton <want_my_s2000@hotmail.com>
2015-11-12 11:26:37 -0500

    Revert "SBjb -- Only add if row exists"
    This reverts commit 8a2b333cf83acdc6e82def0d3621f10449a725fd.

--- lib/dullard/reader.rb
+++ lib/dullard/reader.rb
@@ -263,7 +263,7 @@ class Dullard::Sheet
           end
           cell_type = nil
 
-          row << (shared ? string_lookup(value.to_i) : value) unless row.nil?
+          row << (shared ? string_lookup(value.to_i) : value)
           closed = true
         end
       end
